f2dfce9922c97d96e7941b8cf6a200d4bfa33f0e
Johan Hektor <johan.hektor@mau.se>
2021-02-25 10:21:48 +0100

    remove file

--- dddxrd/utils/json_parser.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def parse_parameters(file):
-    """ Parse parameters stored in a json file. Returns a dictionary"""
-    with open(file) as f:
-        parameters = json.load(f)
-    return parameters
