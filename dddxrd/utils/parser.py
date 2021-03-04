e3dd886f4daff81ca71e967e57f0cf8438092c75
Johan Hektor <johan.hektor@mau.se>
2021-03-04 16:43:46 +0100

    parser for fio files almost completed

--- dddxrd/utils/parser.py
+++ dddxrd/utils/parser.py
@@ -1,10 +1,35 @@
 import json
 import yaml
 import os
+import numpy as np
 
-def parse_fio(file):
-    """ Parse a .fio file from the fastsweep macro at P21.2"""
+def parse_fio(file,nchannels=1):
+    """ Parse a .fio file from the fastsweep macro at P21.2. 
+    Not tested on multiple channel sweeps"""
+    scaninfo = {} #dictonary to store scan info in
+    colnames=[]
+    colformats=[]
+    with open(file) as f:
+        for i,line in enumerate(f):
+            if line.startswith('fastsweep'):
+                scaninfo['nbr_images'] = line.split(':')[1].split('/')[0]
+            elif 'acquisition started' in line:
+                scaninfo['starttime'] = line.split('started at')[-1]
+            elif ' = ' in line:
+                kv = line.split(' = ')
+                try:
+                    scaninfo[kv[0]]=float(kv[1])
+                except ValueError:
+                    scaninfo[kv[0]]=kv[1].rstrip()
+            elif line.startswith(' Col '):
+                colnames.append(line.split(' ')[3])
+            elif line.startswith('  '): #first line of data
+                break
+        headerlines=i
+        #read the data
+        scaninfo['data'] = np.genfromtxt(file,dtype=None, names=colnames,skip_header=headerlines,encoding=None)     
 
+    return scaninfo
 
 def parse_parameters(file):
     """ Parse parameters stored in a json file. Returns a dictionary"""
@@ -37,4 +62,11 @@ def find_padding(pars):
         raise RuntimeError('format string problem, revisit')
     return ndigits
 
-    
+def main():
+    fiofile = '/Users/al8720/Box/projects/Inconel/eh3scan1_00055.fio'
+    info = parse_fio(fiofile)
+    for keys in info:
+        print(keys,info[keys])
+
+if __name__ == "__main__":
+    main()
