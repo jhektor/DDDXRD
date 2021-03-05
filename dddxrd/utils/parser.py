d69b1019037dbd7561868740c1e8efe652a42b92
Johan Hektor <johan.hektor@mau.se>
2021-03-05 10:53:01 +0100

    fioparsing stuff

--- dddxrd/utils/parser.py
+++ dddxrd/utils/parser.py
@@ -9,10 +9,11 @@ def parse_fio(file,nchannels=1):
     scaninfo = {} #dictonary to store scan info in
     colnames=[]
     colformats=[]
+    scaninfo['fiofile'] = file
     with open(file) as f:
         for i,line in enumerate(f):
             if line.startswith('fastsweep'):
-                scaninfo['nbr_images'] = line.split(':')[1].split('/')[0]
+                scaninfo['nbr_images'] = int(line.split(':')[1].split('/')[0])
             elif 'acquisition started' in line:
                 scaninfo['starttime'] = line.split('started at')[-1]
             elif ' = ' in line:
@@ -29,7 +30,43 @@ def parse_fio(file,nchannels=1):
         #read the data
         scaninfo['data'] = np.genfromtxt(file,dtype=None, names=colnames,skip_header=headerlines,encoding=None)     
 
+        #Compute some additional stuff here
+        scaninfo['beamheight'] = float(scaninfo['eh3st']+scaninfo['eh3sb']) #only if eh3 slits define the beam
+        scaninfo['beamwidth'] = float(scaninfo['eh3si']+scaninfo['eh3so'])
+        idx = np.nonzero(scaninfo['data']['type'] == 'exposure')[0][0] # index of first exposure
+        scaninfo['omegastep']=float(np.diff(scaninfo['data']['end'])[0])
+        scaninfo['startomega'] = float(scaninfo['data']['end'][idx]-0.5*scaninfo['omegastep'])
+        scaninfo['filetype'] = '.{}'.format(str(scaninfo['data']['filename']).split('.')[-1][:-2])
+        scaninfo['first_image'] = int(scaninfo['data']['filename'][idx].split(scaninfo['filetype'])[0].split('_')[-1]) #this is not general
+        # Not general
+        stemlist = scaninfo['data']['filename'][idx].split(scaninfo['filetype'])[0].split('_')[:-1] #this is not general
+        stem = ''
+        for s in stemlist:
+            stem += '{}_'.format(s)
+        scaninfo['stem'] = stem
     return scaninfo
+
+def print_scan_info(scaninfo):
+    """ Print info from a fio file"""
+    for keys in scaninfo:
+        if (keys == 'data') or keys.startswith('hasep212'):
+            continue
+        else:
+            print(keys,scaninfo[keys])
+
+def init_yaml_from_fio(fiofile,yamlfile):
+    """ Initializes a yaml file with parameters from a fio file"""
+    scaninfo = parse_fio(fiofile)
+    yaml_dict={}
+    yaml_dict['image_path'] = scaninfo['channel1_FileDir1']
+    yaml_dict['first_image'] = scaninfo['first_image']
+    yaml_dict['image_stem'] = scaninfo['stem']
+    yaml_dict['nbr_images'] = scaninfo['nbr_images']
+    yaml_dict['filetype'] = scaninfo['filetype']
+    yaml_dict['omegastep'] = scaninfo['omegastep']
+    yaml_dict['startomega'] = scaninfo['startomega']
+    with open(yamlfile, 'w') as file:
+        yaml.dump(yaml_dict, file)
 
 def parse_parameters(file):
     """ Parse parameters stored in a json file. Returns a dictionary"""
@@ -65,8 +102,9 @@ def find_padding(pars):
 def main():
     fiofile = '/Users/al8720/Box/projects/Inconel/eh3scan1_00055.fio'
     info = parse_fio(fiofile)
-    for keys in info:
-        print(keys,info[keys])
+    print_scan_info(info)
+    yamlfile = '/Users/al8720/Box/projects/Inconel/peaksearch_fio.yaml'
+    init_yaml_from_fio(fiofile,yamlfile)
 
 if __name__ == "__main__":
     main()
