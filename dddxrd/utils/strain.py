2360d3c244de2b58096c6beb6df1d7f28d5d651c
Johan Hektor <johan.hektor@mau.se>
2021-04-07 21:18:45 +0200

    stresses

--- dddxrd/utils/strain.py
+++ dddxrd/utils/strain.py
@@ -2,6 +2,7 @@ import numpy as np
 import xfab.tools
 from ImageD11 import finite_strain as fs
 import matplotlib.pyplot as plt
+import dddxrd.utils.crystallography as cry
 
 def matrix_to_voigt(e):
     """ Returns the matrix in Voigt notation [xx,yy,zz,yz,xz,xy]"""
@@ -24,7 +25,7 @@ def calc_strain(ubi,d0,mr=1,ml=-1,finite_strain=True):
     assert (d0[3]==90) and (d0[3]==90) and (d0[3]==90), "Finite strains are only for cubic refence cells for now"
     ub0 = np.linalg.inv(ubi0)
     dg = fs.DeformationGradientTensor( ubi,ub0 )
-    F=dg.F
+    #F=dg.F
     #E = 0.5*(np.dot(F.T,F)-np.eye(3))
     #e = 0.5*(np.eye(3)-np.dot(np.linalg.inv(F).T,np.linalg.inv(F)))
     E = dg.finite_strain_ref(m=mr) # strain in crystal
@@ -36,6 +37,7 @@ def tensor_invariants(e):
     If the tensor is strain/stress:
     I1 is the volumetric strain/stress
     J2 is the magnitude of shear strain/stress"""
+    e = np.array(e)
     if e.ndim==1:
         e = voight_to_matrix(e)
     I1 = np.trace(e)
@@ -63,6 +65,7 @@ def _fill_C9(cs):
     C[4,:] = [0,0,0,0,cs[4],0]
     C[5,:] = [0,0,0,0,0,cs[5]]
     return C
+
 def elasticty_matrix(cs):
     """ Returns the 6x6 elasticity matrix from the coefficients cs. The lengts of coeff determines how the matrix looks:
     Isotropic: cs = [K,mu]
@@ -90,5 +93,36 @@ def elasticty_matrix(cs):
         raise ValueError("C matrix with {:d} independent parameters is not implemented.".format(len(cs)))
     return C
  
-def calc_stress(C,E):
-    return np.dot(C,E)
+def calc_stress(C,E,PK2=True, cauchy=False, F=None):
+    """Calculates the 2nd Piola-Kirchoff and/or Cauchy stress.
+    Second Piola Kirchoff is:
+    S = C:E and is defined in the crystal frame.
+    Cauchy stress is:
+    sigma = (1/det(F))*F*S*F^T) and is defined in the sample frame"""
+    S = np.dot(C,E) # 2nd Piola-Kirchoff
+    if cauchy:
+        assert F is not None, "Need to supply the deformation gradient for computing Cauchy stress"
+        S = voight_to_matrix(S)
+        J = np.linalg.det(F)
+        sigma = (1./J)*np.dot(F,np.dot(S,F.T))
+        #sigma =  matrix_to_voigt(sigma)
+    sigma = matrix_to_voigt(sigma)
+    S = matrix_to_voigt(S)
+    if PK2:
+        if cauchy:
+            return S, sigma
+        else:
+            return S
+    else:
+        return sigma
+
+
+def strain_energy_density(S,E):
+    """ Calculates the strain energy density as W=0.5*(S:E)."""
+    S = np.array(S)
+    E = np.array(E)
+    if S.ndim == 1:
+        S = voight_to_matrix(S)
+    if E.ndim == 1:
+        E = voight_to_matrix(E)
+    return 0.5*np.tensordot(S,E,axes=2)
